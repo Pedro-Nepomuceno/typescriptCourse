36f6043b4e24a344c4a6bf0b531a5077eb560690
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-02-12 13:37:48 -0800

    function annotation

--- index.ts
+++ index.ts
@@ -9,3 +9,9 @@ console.log(person);
 
 // anotations are used to specify a data type of a variable, parameter, function return value and other types of values.
 //annotation help developers catch error early in development
+
+//TYPE INFERENCE is a feature in Typescript compiled, it automatically infer the type of variable if we dont explicitly assign to it
+
+function someString(str: string): void {
+  console.log(`some message here ${str}`);
+}
