7e303e46e1627e70b2f4a2f3042164153184790e
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-02-28 13:34:16 -0800

    type narrowing

--- index.ts
+++ index.ts
@@ -198,3 +198,13 @@ function filterArray<type>(
 const numArray = [1, 3, 4, 5, 6, 7, 8];
 const evenNumber = filterArray<number>(numArray, (num) => num % 2 === 0);
 console.log(evenNumber);
+
+// type narrowing
+
+type myType = string | number;
+
+function narrowingType(value: myType): void {
+  if (typeof value === "string") {
+    console.log(value.toUpperCase());
+  }
+}
