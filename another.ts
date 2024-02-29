2ef4155bd06a1b09da8b542a3125d463b1fdbb5b
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-02-29 13:22:17 -0800

    set Axiosresponse

--- another.ts
+++ another.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosResponse } from "axios";
+
 const numero: number = 2345;
 
 const ArrowFunction = (x: number, y: number): number => x + y;
@@ -5,3 +7,13 @@ const ArrowFunction = (x: number, y: number): number => x + y;
 const addNumero = ArrowFunction(2, 3);
 console.log(addNumero);
 2;
+
+interface Todo {}
+
+const fectchData = async () => {
+  try {
+    const response = AxiosResponse<Todo>;
+  } catch (error) {
+    console.log(error);
+  }
+};
